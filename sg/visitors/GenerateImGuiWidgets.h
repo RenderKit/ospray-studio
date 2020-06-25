f49525a938edc33a87cd20baa567b97dc18ababf
Alok Hota <alok.hota@intel.com>
2020-06-25 02:17:40 -0500

    add imgui widgets for all specialized light params

--- sg/visitors/GenerateImGuiWidgets.h
+++ sg/visitors/GenerateImGuiWidgets.h
@@ -37,17 +37,79 @@ namespace ospray {
     {
       // ALOK: used to copy and assign values from/to children.
       // if we don't, sg doesn't know the node was updated
+      static bool b;
       static float f1;
       static vec3f f3;
 
       if (ImGui::TreeNode(node.name().c_str())) {
         if (node.type() == NodeType::LIGHT) {
+          b = node["visible"].valueAs<bool>();
+          if (ImGui::Checkbox("visible", &b))
+            node["visible"].setValue(b);
+
           f1 = node["intensity"].valueAs<float>();
-          if (ImGui::SliderFloat("intensity", &f1, 0.f, 1.f))
+          if (ImGui::DragFloat("intensity", &f1, 0.1f, 0.0f, 0.0f, "%.1f"))
             node["intensity"].setValue(f1);
+
           f3 = node["color"].valueAs<vec3f>();
           if (ImGui::ColorEdit3("color", f3))
             node["color"].setValue(f3);
+
+          std::string lightType = node["type"].valueAs<std::string>();
+          if (lightType == "distant") {
+            f3 = node["direction"].valueAs<vec3f>();
+            if (ImGui::SliderFloat3("direction", f3, -1.f, 1.f))
+              node["direction"].setValue(f3);
+
+            f1 = node["angularDiameter"].valueAs<float>();
+            if (ImGui::SliderFloat("angular diameter", &f1, 0.f, 1.f))
+              node["angularDiameter"].setValue(f1);
+
+          } else if (lightType == "sphere") {
+            f3 = node["position"].valueAs<vec3f>();
+            if (ImGui::DragFloat3("position", f3, 0.1f))
+              node["position"].setValue(f3);
+
+            f1 = node["radius"].valueAs<float>();
+            if (ImGui::DragFloat("radius", &f1, 0.1f, 0.f, 0.f, "%.1f"))
+              node["radius"].setValue(f1);
+
+          } else if (lightType == "spot") {
+            f3 = node["position"].valueAs<vec3f>();
+            if (ImGui::DragFloat3("position", f3, 0.1f))
+              node["position"].setValue(f3);
+
+            f3 = node["direction"].valueAs<vec3f>();
+            if (ImGui::SliderFloat3("direction", f3, -1.f, 1.f))
+              node["direction"].setValue(f3);
+
+            f1 = node["openingAngle"].valueAs<float>();
+            if (ImGui::SliderFloat(
+                    "opening angle", &f1, 0.f, 180.f, "%.0f", 1.f))
+              node["openingAngle"].setValue(f1);
+
+            f1 = node["penumbraAngle"].valueAs<float>();
+            if (ImGui::SliderFloat(
+                    "penumbra angle", &f1, 0.f, 180.f, "%.0f", 1.f))
+              node["penumbraAngle"].setValue(f1);
+
+            f1 = node["radius"].valueAs<float>();
+            if (ImGui::DragFloat("radius", &f1, 0.1f, 0.f, 0.f, "%.1f"))
+              node["radius"].setValue(f1);
+
+          } else if (lightType == "quad") {
+            f3 = node["position"].valueAs<vec3f>();
+            if (ImGui::DragFloat3("position", f3, 0.1f))
+              node["position"].setValue(f3);
+
+            f3 = node["edge1"].valueAs<vec3f>();
+            if (ImGui::DragFloat3("edge 1", f3, 0.1f))
+              node["edge1"].setValue(f3);
+
+            f3 = node["edge2"].valueAs<vec3f>();
+            if (ImGui::DragFloat3("edge 2", f3, 0.1f))
+              node["edge2"].setValue(f3);
+          }
 
           return false;
         }
