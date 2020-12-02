35ef7fdaffc08bb74d6e8ad1cd80ce1ed6c2116d
Krzysztof Raszkowski <krzysztof.raszkowski@intel.com>
2020-12-02 11:49:23 +0100

    ci: increase kw test script severity to Error,Critical

--- gitlab/test-kw.sh
+++ gitlab/test-kw.sh
@@ -8,7 +8,7 @@ apt-get update -y && apt-get install curl jq -y
 KW_CRITICAL_OUTPUT_PATH=/tmp/critical.out
 KW_BUILD_NUMBER=$(cat $CI_PROJECT_DIR/kw_build_number)
 echo "Checking for critical issues in $KW_BUILD_NUMBER ..."
-curl -k -f --data "action=search&project=$KW_PROJECT_NAME&query=build:'$KW_BUILD_NUMBER'%20severity:Critical%20status:Analyze,Fix&user=$KW_USER&ltoken=$KW_LTOKEN" http://$KW_SERVER_IP:$KW_SERVER_PORT/review/api -o $KW_CRITICAL_OUTPUT_PATH
+curl -k -f --data "action=search&project=$KW_PROJECT_NAME&query=build:'$KW_BUILD_NUMBER'%20severity:Error,Critical%20status:Analyze,Fix&user=$KW_USER&ltoken=$KW_LTOKEN" http://$KW_SERVER_IP:$KW_SERVER_PORT/review/api -o $KW_CRITICAL_OUTPUT_PATH
 ISSUES_COUNT=$(cat $KW_CRITICAL_OUTPUT_PATH | wc -l)
 if [ "$ISSUES_COUNT" -gt "0" ]; then
         echo "Issues found - $ISSUES_COUNT in $KW_BUILD_NUMBER";
