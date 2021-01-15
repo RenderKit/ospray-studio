d0d24d9df712fa334fcbfe09e530bfe05f8ef09a
Isha Sharma <isha.sharma@intel.com>
2021-01-16 00:18:16 +0100

    rename lights to LightsManager

---
+++ sg/scene/lights/LightsManager.cpp
@@ -0,0 +1,111 @@
+// Copyright 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#include "LightsManager.h"
+#include "../../sg/renderer/Renderer.h"
+
+namespace ospray {
+  namespace sg {
+
+    OSP_REGISTER_SG_NODE_NAME(Lights, lights);
+
+    Lights::Lights()
+    {
+      lightNames.push_back("ambient");
+      createChild("ambient", "ambient");
+    }
+
+    NodeType Lights::type() const
+    {
+      return NodeType::LIGHTS;
+    }
+
+    bool Lights::lightExists(std::string name)
+    {
+      auto found = std::find(lightNames.begin(), lightNames.end(), name);
+      return (found != lightNames.end());
+    }
+
+    bool Lights::addLight(std::string name, std::string lightType)
+    {
+      if (name == "" || lightExists(name))
+        return false;
+
+      lightNames.push_back(name);
+      createChild(name, lightType);
+      return true;
+    }
+
+    bool Lights::addLight(NodePtr light)
+    {
+      if (lightExists(light->name()))
+        return false;
+
+      lightNames.push_back(light->name());
+      add(light);
+      return true;
+    }
+
+    void Lights::addLights(std::vector<NodePtr> &lights)
+    {
+      for (auto &l : lights) {
+        if (lightExists(l->name()))
+          continue;
+
+        lightNames.push_back(l->name());
+        add(l);
+      }
+    }
+
+    bool Lights::removeLight(std::string name)
+    {
+      if (name == "" || !lightExists(name))
+        return false;
+
+      remove(name);
+      auto found = std::find(lightNames.begin(), lightNames.end(), name);
+      lightNames.erase(found);
+
+      // XXX FIX: this markAsModified is not doing anything ?!?!
+      markAsModified();
+      isStubborn = true; // XXX HACK, until we get markAsModified working here
+      return true;
+    }
+
+    void Lights::preCommit()
+    {
+      isStubborn = false;
+      cppLightObjects.clear();
+
+      for (auto &name : lightNames) {
+        auto &l = child(name);
+        if (l.subType() == "hdri") {
+          auto &frame = currentWorld->parents().front();
+          auto &renderer = frame->childAs<sg::Renderer>("renderer");
+          renderer["backgroundColor"] = vec4f(0.f);
+        }
+
+        cppLightObjects.emplace_back(l.valueAs<cpp::Light>());
+      }
+    }
+
+    void Lights::postCommit()
+    {
+    }
+
+    void Lights::updateWorld(World &world)
+    {
+      currentWorld = &world;
+      // Commit lightsManager changes then apply lightObjects on the world.
+      commit();
+
+      if (!cppLightObjects.empty())
+        world.handle().setParam("light", cpp::CopiedData(cppLightObjects));
+      else
+        world.handle().removeParam("light");
+
+      world.handle().commit();
+    }
+
+  }  // namespace sg
+}  // namespace ospray
---
+++ sg/scene/lights/LightsManager.h
@@ -0,0 +1,47 @@
+// Copyright 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "../../Node.h"
+#include "../World.h"
+
+namespace ospray {
+  namespace sg {
+
+    /**
+     * Lights node - manages lights in the world
+     *
+     * This node maintains a list of all lights in the world. It holds a list of
+     * Light nodes which is passed to the world on commit. This list lets us
+     * add/remove lights without adding unrelated functionality to the World
+     * node
+     */
+    struct OSPSG_INTERFACE Lights : public OSPNode<cpp::Light, NodeType::LIGHTS>
+    {
+      Lights();
+      ~Lights() override = default;
+      NodeType type() const override;
+      bool lightExists(std::string name);
+      bool addLight(std::string name, std::string lightType);
+      bool addLight(NodePtr light);
+      void addLights(std::vector<NodePtr> &lights);
+      bool removeLight(std::string name);
+
+      void updateWorld(World &world);
+
+     // protected:
+      std::vector<std::string> lightNames;
+      std::vector<cpp::Light> cppLightObjects = {};
+
+      bool isStubborn{false}; // XXX HACK, until markAsModified is reliable
+
+      virtual void preCommit() override;
+      virtual void postCommit() override;
+
+      private:
+       World* currentWorld;
+    };
+
+  }  // namespace sg
+}  // namespace ospray
