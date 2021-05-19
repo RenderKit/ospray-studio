2bafbf9a7d4c5f6b7c4f770762b5c6a6f2ac36e1
Bruce Cherniak <bruce.cherniak@intel.com>
2021-05-18 22:43:49 -0500

    Reworked Particle Volume generator to make it more interactive.
    - Also needed to add a new ImGuiWidget for Range types (1i and 1f currently).

--- sg/generator/ParticleVol.cpp
+++ sg/generator/ParticleVol.cpp
@@ -21,10 +21,6 @@ struct ParticleVol : public Generator
   std::vector<float> radius;
   std::vector<float> weight;
 
-  float clampMaxCumulativeValue;
-  float radiusSupportFactor;
-  bool estimateValueRanges;
-
   void generateData() override;
 };
 
@@ -37,9 +33,19 @@ ParticleVol::ParticleVol()
 {
   auto &parameters = child("parameters");
 
-  parameters.createChild("dimensions", "vec3i", vec3i(10));
-  parameters.createChild("gridOrigin", "vec3f", vec3f(-1.f));
-  parameters.createChild("gridSpacing", "vec3f", vec3f(1.f));
+  parameters.createChild("dimensions", "vec3i", "grid size", vec3i(10));
+  parameters.createChild("weightRange", "range1f", range1f(0.5f, 1.5f));
+  parameters.createChild("numParticles", "int", 100);
+
+  parameters.createChild(
+      "radiusSupportFactor", "float", "particle radius multiplier", 3.f);
+  parameters.createChild("estimateValueRanges",
+      "bool",
+      "heuristic estimation of value ranges",
+      true);
+
+  parameters["weightRange"].setMinMax(0.f, 10.f);
+  parameters["numParticles"].setMinMax(1, 10000);
 
   auto &xfm = createChild("xfm", "transform");
 }
@@ -50,41 +56,55 @@ void ParticleVol::generateData()
   int32_t randomSeed = 0;
   std::mt19937 gen(randomSeed);
 
-  range1f weightRange(0.5f, 1.5f);
-  box3f bounds(vec3f(0.f), vec3f(10.f));
+  auto &parameters = child("parameters");
 
-  std::uniform_real_distribution<float> centerDistribution_x(bounds.lower.x, bounds.upper.x);
-  std::uniform_real_distribution<float> centerDistribution_y(bounds.lower.y, bounds.upper.y);
-  std::uniform_real_distribution<float> centerDistribution_z(bounds.lower.z, bounds.upper.z);
+  auto numParticles = parameters["numParticles"].valueAs<int>();
+  auto radiusSupportFactor = parameters["radiusSupportFactor"].valueAs<float>();
+  auto estimateValueRanges = parameters["estimateValueRanges"].valueAs<bool>();
+
+  auto weightRange = parameters["weightRange"].valueAs<range1f>();
+  auto dimensions = parameters["dimensions"].valueAs<vec3i>();
+
+  box3f bounds(vec3f(0.f), dimensions);
+
+  std::uniform_real_distribution<float> centerDistribution_x(
+      bounds.lower.x, bounds.upper.x);
+  std::uniform_real_distribution<float> centerDistribution_y(
+      bounds.lower.y, bounds.upper.y);
+  std::uniform_real_distribution<float> centerDistribution_z(
+      bounds.lower.z, bounds.upper.z);
   std::uniform_real_distribution<float> radiusDistribution(.25f, 1.0f);
-  std::uniform_real_distribution<float> weightDistribution(weightRange.lower, weightRange.upper);
-
-  size_t numParticles = 100;
+  std::uniform_real_distribution<float> weightDistribution(
+      weightRange.lower, weightRange.upper);
 
   position.resize(numParticles);
   radius.resize(numParticles);
   weight.resize(numParticles);
 
-  tasking::parallel_for(numParticles, [&](size_t i) {
-    position[i] = vec3f(centerDistribution_x(gen), centerDistribution_y(gen), centerDistribution_z(gen));
+  tasking::parallel_for(numParticles, [&](int i) {
+    position[i] = vec3f(centerDistribution_x(gen),
+        centerDistribution_y(gen),
+        centerDistribution_z(gen));
     radius[i] = radiusDistribution(gen);
     weight[i] = weightDistribution(gen);
   });
-
-  clampMaxCumulativeValue = weightRange.upper;
-  radiusSupportFactor = 3.f;
-  estimateValueRanges = false;
 
   auto &xfm = child("xfm");
   auto &tf = xfm.createChild("transferFunction", "transfer_function_jet");
 
   auto &pvol = tf.createChild("pvol", "volume_particle");
+  pvol.createChild("gridOrigin", "vec3f", vec3f(-1.f));
+  pvol.createChild("gridSpacing", "vec3f", 1.f / dimensions);
+
   pvol.createChildData("particle.position", position, true);
   pvol.createChildData("particle.radius", radius, true);
   pvol.createChildData("particle.weight", weight, true);
-  pvol.createChild("clampMaxCumulativeValue", "float", clampMaxCumulativeValue);
+  pvol.createChild("clampMaxCumulativeValue", "float", weightRange.upper);
+  pvol["clampMaxCumulativeValue"].setMinMax(0.f, weightRange.upper);
   pvol.createChild("radiusSupportFactor", "float", radiusSupportFactor);
+  pvol["radiusSupportFactor"].setMinMax(0.1f, 6.f);
   pvol.createChild("estimateValueRanges", "bool", estimateValueRanges);
+  pvol.createChild("maxIteratorDepth", "int", 6);
 }
 
 } // namespace sg
