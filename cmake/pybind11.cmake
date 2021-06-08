ed55af050f921562e1c9bf9f5a77d4c89964b7ac
Trevor Thomson <trevor.thomson@intel.com>
2021-06-07 20:22:55 -0400

    Enable python bindings via pybind11

---
+++ cmake/pybind11.cmake
@@ -0,0 +1,39 @@
+
+##
+## Configure
+##
+
+if(NOT DEFINED PYBIND11_VERSION)
+  set(PYBIND11_VERSION 2.6.2)
+endif()
+
+##
+## Implementation
+##
+
+message(STATUS "Getting pybind11 ${PYBIND11_VERSION}...")
+
+find_package(Python3 COMPONENTS Interpreter Development REQUIRED)
+find_package(pybind11 ${PYBIND11_VERSION} QUIET)
+
+if(NOT "${pybind11_FOUND}")
+
+  message(STATUS "Downloading pybind11 ${PYBIND11_VERSION}...")
+
+  include(FetchContent)
+
+  set(_PYBIND11_ARCHIVE_EXT "zip")
+
+  FetchContent_Declare(
+    pybind11
+    URL "https://github.com/pybind/pybind11/archive/refs/tags/v${PYBIND11_VERSION}.${_PYBIND11_ARCHIVE_EXT}"
+  )
+  ## Bypass FetchContent_MakeAvailable() shortcut to disable install
+  FetchContent_GetProperties(pybind11)
+  if(NOT pybind11_POPULATED)
+    FetchContent_Populate(pybind11)
+    ## the subdir will still be built since targets depend on it, but it won't be installed
+    add_subdirectory(${pybind11_SOURCE_DIR} ${pybind11_BUILD_DIR} EXCLUDE_FROM_ALL)
+  endif()
+
+endif()
