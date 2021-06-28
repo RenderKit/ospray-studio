5398c5df859fab22be992b2e0cb00a0e8f16ddbd
Trevor Thomson <trevor.thomson@intel.com>
2021-06-28 10:10:50 -0400

    Cache CXX11_ABI setting when downloading OSPRay

--- cmake/ospray.cmake
+++ cmake/ospray.cmake
@@ -40,8 +40,8 @@ else()
 
     if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
         ## The pre-built library was compiled pre-c++-11, so disable the CXX11 ABI
-        set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_GLIBCXX_USE_CXX11_ABI=0")
-        message (STATUS "setting -D_GLIBCXX_USE_CXX11_ABI=0")
+        set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_GLIBCXX_USE_CXX11_ABI=0" CACHE STRING "Disable CXX11 ABI" FORCE)
+        message (STATUS "Setting -D_GLIBCXX_USE_CXX11_ABI=0")
     endif()
 
     ## The pre-build library was compiled with TBB 2021.2.0, so ensure the same TBB is found
