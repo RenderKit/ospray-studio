4fc98c8999c0eaffe9afda1499cb1733af205c4f
Alok Hota <alok.hota@intel.com>
2021-09-23 16:17:10 -0500

    add default parameters to OBJ material

--- sg/renderer/materials/OBJ.cpp
+++ sg/renderer/materials/OBJ.cpp
@@ -16,7 +16,14 @@ namespace ospray {
 
   // MaterialOBJ definitions //////////////////////////////////////////////////
 
-  MaterialOBJ::MaterialOBJ() : Material("obj") {}
+  MaterialOBJ::MaterialOBJ() : Material("obj") {
+    createChild("kd", "rgb", "diffuse color", vec3f(0.8f));
+    createChild("ks", "rgb", "specular color", vec3f(0.f));
+    createChild("ns", "float", "shininess (Phong) [2-1e5]", 10.f)
+        .setMinMax(2.f, 1e5f);
+    createChild("d", "float", "opacity [0-1]", 1.f);
+    createChild("tf", "rgb", "transparency filter color", vec3f(0.f));
+  }
 
   }  // namespace sg
 } // namespace ospray
