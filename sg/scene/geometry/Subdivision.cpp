23ecaf097aeb1b21079bc3fd4acccfd549e50293
Bruce Cherniak <bruce.cherniak@intel.com>
2021-12-08 17:34:40 -0600

    Add subdivision geometry type, as used by USD importer

---
+++ sg/scene/geometry/Subdivision.cpp
@@ -0,0 +1,23 @@
+// Copyright 2009-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+#include "Geometry.h"
+
+namespace ospray {
+namespace sg {
+
+struct OSPSG_INTERFACE Subdivision : public Geometry
+{
+  Subdivision();
+  virtual ~Subdivision() override = default;
+};
+
+OSP_REGISTER_SG_NODE_NAME(Subdivision, geometry_subdivision);
+
+// Subdivision definitions //////////////////////////////////////////////////
+
+Subdivision::Subdivision() : Geometry("subdivision") {}
+
+} // namespace sg
+} // namespace ospray
+
