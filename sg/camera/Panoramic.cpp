33f17353cb351226e05f72935b0626874a44607d
Johannes Günther <johannes.guenther@intel.com>
2024-06-28 15:27:24 +0200

    Consistent camera parameters

--- sg/camera/Panoramic.cpp
+++ sg/camera/Panoramic.cpp
@@ -4,27 +4,29 @@
 #include "Camera.h"
 
 namespace ospray {
-  namespace sg {
+namespace sg {
 
-  struct Panoramic : public Camera
-  {
-    Panoramic();
-  };
+struct Panoramic : public Camera
+{
+  Panoramic();
+};
 
-  Panoramic::Panoramic() : Camera("panoramic")
-  {
-    createChild("interpupillaryDistance",
-                "float",
-                "Distance between left and right eye for stereo mode",
-                0.0635f);
-    child("interpupillaryDistance").setMinMax(0.f, 0.1f);
-    createChild("stereoMode",
-                "OSPStereoMode",
-                "0=none, 1=left, 2=right, 3=side-by-side, 4=top-bottom",
-                OSP_STEREO_NONE);
-  }
+Panoramic::Panoramic() : Camera("panoramic")
+{
+  createChild("stereoMode",
+      "OSPStereoMode",
+      "0=none, 1=left, 2=right, 3=side-by-side, 4=top-bottom",
+      OSP_STEREO_NONE);
+  createChild("interpupillaryDistance",
+      "float",
+      "Distance between left and right eye for stereo mode",
+      0.0635f);
 
-  OSP_REGISTER_SG_NODE_NAME(Panoramic, camera_panoramic);
+  child("stereoMode").setMinMax(OSP_STEREO_NONE, OSP_STEREO_TOP_BOTTOM);
+  child("interpupillaryDistance").setMinMax(0.f, 0.1f);
+}
 
-  }  // namespace sg
+OSP_REGISTER_SG_NODE_NAME(Panoramic, camera_panoramic);
+
+} // namespace sg
 } // namespace ospray
